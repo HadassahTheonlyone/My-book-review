8e96f3266e06d2155d50c52d2791dd0d26afbe86
Hads <94758037+HadassahTheonlyone@users.noreply.github.com>
2021-11-20 13:01:13 -0500

    Add files via upload

---
+++ firstwebsite/index.js
@@ -0,0 +1,23 @@
+console.log("Javascript is connected")
+alert("Welcome to Haddies Book Recommendation club")
+
+
+document.getElementById("myButton").addEventListener("click", function () {
+    location.href = "./bookreviews/meetthetwitches.html";
+});
+
+document.getElementById("myButton1").addEventListener("click", function () {
+    location.href = "readme.html";
+});
+document.getElementById("myButton2").addEventListener("click", function () {
+    location.href = "readme.html";
+});
+document.getElementById("myButton3").addEventListener("click", function () {
+    location.href = "readme.html";
+});
+document.getElementById("myButton4").addEventListener("click", function () {
+    location.href = "readme.html";
+});
+document.getElementById("myButton5").addEventListener("click", function () {
+    location.href = "readme.html";
+});
